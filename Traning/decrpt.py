4759cd174ad0cf12288157ea241e3f08d403e956
HayaL0141 <88240104+HayaL0141@users.noreply.github.com>
2024-07-01 08:04:57 +0900

    Add files via upload

---
+++ Traning/decrpt.py
@@ -0,0 +1,14 @@
+print("Hello, Contosoville")
+# This is comment that won`t be intepreted as a comand
+
+# Asociate variable town with the value "Contosoville"
+town = "Contosoville"
+# Print a message about the secret location
+print ("The town I am looking for is " + town )
+
+# Define a power (funciton) to chant a phrase
+def chant (phrase):
+    #Glue three copies together and print it as a message
+    print(phrase + phrase + phrase) 
+# Invoke the power to chant on the phrase "Contosoville"
+chant("Contosoville ")
