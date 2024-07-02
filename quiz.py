ec29d25b2794f9237316f92034d4f30bdbf4277e
HayaL0141 <88240104+HayaL0141@users.noreply.github.com>
2024-07-02 10:02:08 +0900

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -4,4 +4,17 @@ print("Hello,Contosoville!")
 # Use a variable named year to "remembar" the value 1990
 year = 1990
 
-print(year)
+print(f"The yesr is {year}...")
+
+year = year +36
+
+# print a message to see what year is now
+print(f"The year is now {year}")
+
+# if we're in 1990
+if year == 1990:
+    print("I left you a message on your answering machine!")
+# if we'ew in 2026
+if year == 2026:
+    print("I sent you a text message!")
+
