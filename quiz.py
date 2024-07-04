f59fb202b9f31bedca5378c7d219f0148fa5f93e
HayaL0141 <88240104+HayaL0141@users.noreply.github.com>
2024-07-04 09:15:22 +0900

    中間

--- quiz.py
+++ quiz.py
@@ -28,3 +28,27 @@ print( f"You chose {activity}")
 # if they chose reading a book
 if activity == "A":
     print("Nice choice!")
+elif activity == "B":
+    print("Sound fun!")
+else :
+    print("You must type A or B, let's just say you like to read.")
+    activity == "A"
+
+# ask the candidate a second qustion
+job = input("What's your dream job?\n(A) Curator at the Smithsonian\n(B) Running a business\n")
+if job == "A":
+    print( "Curator, nice choice")
+elif job =="B":
+    print( "Running a business? Sounds fun!")
+else:
+    print("You must type A or B, let's just say you want to be a curator at the Smithsonian.")
+
+# ask the candidate a third question
+value = input("What's more important?\n(A) Money\n(B) Love\n")
+if value =="A":
+    print( "Money, nice choice")
+elif value == "B":
+    print( "Love? Sounds fun!")
+else:
+    print("You must type A or B, let's just say money is more important to you.")
+    value = "A"
